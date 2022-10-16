3737cb59727f6af2dcf19387b28ccead440a3f95
Thunderbird93 <antircristian@gmail.com>
2022-10-16 21:15:19 +0200

    add const mail

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
+*/
 
--
--
--
+const utente = ["mail@mail.com", "mail@mail.it", "mail@mail.co.uk"]
 
+/*
 **Gioco dei dadi**
 
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
@@ -26,6 +26,7 @@ let y = Math.floor(Math.random() * 6) + 1;
 console.log(x);
 console.log(y);
 
+//Funzione che dichiara il vincitore
 if (x == y) {
     console.log("Try Again")
 } else if (x > y) {
